b0b688cd28022cdd2025f7d6da20fe75fcca4b68
floor-licker <kingofthecoast78@gmail.com>
2025-12-05 12:53:31 -0500

    fix: resolve proto generation compilation errors, add prost-types 0.13 dependency to match prost version, update generated module exports to include all IBC core services (client connection channel commitment), add Cosmos dependencies (base query ics23 upgrade) and Google API protos, successfully compile all generated gRPC client code

--- crates/relayer/src/chain/cardano/generated/mod.rs
+++ crates/relayer/src/chain/cardano/generated/mod.rs
@@ -4,10 +4,64 @@
 #![allow(clippy::all)]
 #![allow(warnings)]
 
+// Cosmos dependencies
+pub mod cosmos_proto {
+    include!("cosmos_proto.rs");
+}
+
+pub mod cosmos {
+    pub mod base {
+        pub mod query {
+            pub mod v1beta1 {
+                include!("cosmos.base.query.v1beta1.rs");
+            }
+        }
+    }
+    pub mod ics23 {
+        pub mod v1 {
+            include!("cosmos.ics23.v1.rs");
+        }
+    }
+    pub mod upgrade {
+        pub mod v1beta1 {
+            include!("cosmos.upgrade.v1beta1.rs");
+        }
+    }
+}
+
+pub mod google {
+    pub mod api {
+        include!("google.api.rs");
+    }
+}
+
+// IBC core modules
 pub mod ibc {
     pub mod cardano {
         pub mod v1 {
             include!("ibc.cardano.v1.rs");
+        }
+    }
+    pub mod core {
+        pub mod client {
+            pub mod v1 {
+                include!("ibc.core.client.v1.rs");
+            }
+        }
+        pub mod connection {
+            pub mod v1 {
+                include!("ibc.core.connection.v1.rs");
+            }
+        }
+        pub mod channel {
+            pub mod v1 {
+                include!("ibc.core.channel.v1.rs");
+            }
+        }
+        pub mod commitment {
+            pub mod v1 {
+                include!("ibc.core.commitment.v1.rs");
+            }
         }
     }
 }
